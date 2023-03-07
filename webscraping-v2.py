62d17962f4db5c50d8794587de3f7afa56fec4b3
putriromhadhoni <putriromhadhoni12@gmail.com>
2023-03-08 05:58:00 +0700

    adding pagination

--- webscraping-v2.py
+++ webscraping-v2.py
@@ -29,31 +29,37 @@ soup.find/soup.find_all('h1')
 #     file.write(transkrip)
 
 root = "https://subslikescript.com"
-web = f'{root}/movies'
-res = requests.get(web)
-soup1 = BeautifulSoup(res.text, 'lxml')
-
-#MULTIPLE LINK
-
-box1 = soup1.find('article', class_ = 'main-article')
-links = []
-for link in box1.find_all('a', href = True):
-    links.append(link['href'])
-# print(links)
-
 path = '/Users/putriromhadhoni/Documents/Learning/analytics_eng/web-scraping-res'
 
-for link in links:
-    web1 = f'{root}/{link}'
-    ress = requests.get(web1)
-    soup2 = BeautifulSoup(ress.text, 'lxml')
-
-    box = soup2.find('article', class_ = 'main-article')
-    title = box.find('h1').get_text()
-    transkrip = box.find('div', class_ = 'full-script').get_text(strip = True, separator = ' ')
-
-    # with open(os.path.join(path, f'{title}.txt'), 'w') as file:
-    #     file.write(transkrip)
-
 #pagination
+result = requests.get('https://subslikescript.com/movies_letter-A')
+soup = BeautifulSoup(result.text, 'lxml')
+boxs = soup.find('ul', class_ = 'pagination')
+page = boxs.find_all('li', class_ = 'page-item')[-2].get_text()
 
+for i in range(1,int(page)+1):
+    web = f'{root}/movies_letter-A?page={i}'
+    res = requests.get(web)
+    soup1 = BeautifulSoup(res.text, 'lxml')
+
+    #MULTIPLE LINK
+
+    box1 = soup1.find('article', class_ = 'main-article')
+    links = []
+    for link in box1.find_all('a', href = True):
+        links.append(link['href'])
+
+    for link in links:
+        try:
+            ress = requests.get(f'{root}/{link}')
+            soup2 = BeautifulSoup(ress.text, 'lxml')
+
+            box = soup2.find('article', class_ = 'main-article')
+            title = box.find('h1').get_text()
+            transkrip = box.find('div', class_ = 'full-script').get_text(strip = True, separator = ' ')
+
+            # with open(os.path.join(path, f'{title}.txt'), 'w') as file:
+            #     file.write(transkrip)
+        except:
+            print('link is not working')
+            print(link)
